63ca265b2a31e9d5b8f2256429d588b91fa86a01
Dennis1903 <112455243+Dennis1903@users.noreply.github.com>
2022-09-26 05:29:10 -0700

    Add files via upload

---
+++ CMSC203_ACTIVITY2_FLORITA.java
@@ -0,0 +1,20 @@
+package Florita;
+public class CMSC203_ACTIVITY2_FLORITA {
+    public static void main(String [] args) {
+   	 
+   	String name = "Dennis Florita" ;
+   	int age = 20;
+   	float gpa=  1.74f;
+       String bloodtype = " A ";
+       
+       
+       System.out.println("My name is " + name );
+       System.out.println("I am " + age + " years old ");
+       System.out.println("My GPA is " + gpa );
+       System.out.println("My blood type is" + bloodtype);
+       		
+      
+   	    	 
+   	 
+   	}
+}
